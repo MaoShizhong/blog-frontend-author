e9efad84d7f4e75b9ea5abe206ac5d6b65e5f347
MaoShizhong <122839503+MaoShizhong@users.noreply.github.com>
2023-09-24 02:08:26 +0100

    Set dev server port for authentication testing
    API will check request origin when logging in. CMS will only authorise
    login if the user is an author.
    Main client will authorise any validated user login.

--- src/helpers/domain.ts
+++ src/helpers/domain.ts
@@ -1,3 +1,2 @@
-// const localHost = 'http://localhost:5000';
-const deployed = 'https://mao-blog-api.adaptable.app';
-export const API_DOMAIN = deployed;
+export const API_DOMAIN = 'https://mao-blog-api.adaptable.app';
+// export const API_DOMAIN = 'http://localhost:5000';
